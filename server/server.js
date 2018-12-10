197d620ca69de93fd82cd545ced597b58fedaf3c
Alexander <alexander.botello@g.austincc.edu>
2018-12-09 21:33:53 -0600

    Integrate TravisCI

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+
+// 3rd party middleware
+app.use(cors());
+app.use(bodyParser());
+
+app.listen(5005, () => {
+    console.log("Server listening on port 5005")
+})
+
+module.exports = {
+  app
+}
