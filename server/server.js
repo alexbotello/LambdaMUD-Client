73ca6f1350bffd4dd1bab0e9f4844f7a9ac47c3d
Alexander <alexander.botello@g.austincc.edu>
2018-12-09 22:41:14 -0600

    linting

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 let app = express();
-
 
 // 3rd party middleware
 app.use(cors());
 app.use(bodyParser());
 
 app.listen(5005, () => {
-    console.log("Server listening on port 5005")
-})
+  console.log("Server listening on port 5005");
+});
 
 module.exports = {
   app
-}
+};
