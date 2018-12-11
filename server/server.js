31f01337977692dd56965282c5b7ac6f2c2114b4
Alexander <alexander.botello@g.austincc.edu>
2018-12-11 00:05:34 -0600

    tery

--- server/server.js
+++ server/server.js
@@ -2,17 +2,16 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 
-let app = express();
+let app = express()
 
 // 3rd party middleware
-app.use(cors());
-app.use(bodyParser());
+app.use(cors())
+app.use(bodyParser())
 
 app.listen(5003, () => {
-  console.log("Server listening on port 5005");
-  console.log("lets test if this linter commit works");
-  console.log("hellosds");
-  console.log("blah");
+  console.log("Server listening on port 5005")
+  console.log("lets test if this linter commit works")
+  console.log("hellosds")
 })
 
-export default app;
+export default app
