6e64520d19976a6897c6beff8f90dc918d37df14
Alexander <alexander.botello@g.austincc.edu>
2018-12-10 22:19:18 -0600

    try

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ let app = express();
 
 // 3rd party middleware
 app.use(cors());
-app.use(bodyParser())
+app.use(bodyParser());
 
 app.listen(5003, () => {
-  console.log("Server listening on port 5005")
-  console.log("lets test if this linter commit works")
+  console.log("Server listening on port 5005");
+  console.log("lets test if this linter commit works");
   console.log("hello");
 })
 
