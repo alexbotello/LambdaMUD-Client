8359fcb487f756ef14cc024cac3c0c178fd3a3c1
Alexander <alexander.botello@g.austincc.edu>
2018-12-10 22:04:39 -0600

    hello

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(bodyParser());
 app.listen(5003, () => {
   console.log("Server listening on port 5005");
   console.log("lets test if this linter commit works");
+  console.log("hello");
 });
 
 export default app;
